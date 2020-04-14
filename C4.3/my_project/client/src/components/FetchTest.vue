6cf8dca0eeebb29170be584ba2c54a4eaad2e17d
NIKOLAY VETROV <nvetrov@gmail.com>
2020-04-14 18:00:51 +0300

    Initial commit

---
+++ C4.3/my_project/client/src/components/FetchTest.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const dataURL = 'http://localhost:5000/api/tasks/';
+
+export default {
+  name: 'Fetch',
+  data() {
+    return {
+      msg: 'Before request',
+    };
+  },
+  methods: {
+    getMesasge() {
+      axios.get(dataURL)
+        .then((response) => {
+          console.table(response.data);
+        });
+    },
+  },
+  created() {
+    this.getMesasge();
+  },
+};
+</script>
